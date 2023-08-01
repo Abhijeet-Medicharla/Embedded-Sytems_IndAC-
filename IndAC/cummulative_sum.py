9845e288879ba39b30c13e92080acf560a618c3a
Abhijeet Medicharla <abhijeet.medicharla@gmail.com>
2023-08-01 16:24:15 +0530

    Delete IndAC directory

--- IndAC/cummulative_sum.py
+++
@@ -1,8 +0,0 @@
-a=[]
-n=int(input("Enter number of elements:"))
-for x in range(n):
-    element=int(input("Enter element:"))
-    a.append(element)
-b=[sum(a[0:x+1]) for x in range(0,len(a))]
-print("The original list is:",a)
-print("Cummulative sum is",b)
